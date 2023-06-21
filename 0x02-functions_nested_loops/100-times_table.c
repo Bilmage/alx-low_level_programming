f5d2583b83bdf43a6a51358f316059605a61af16
Nelson <34085153+Bilmage@users.noreply.github.com>
2023-06-21 18:55:38 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,45 +6,45 @@
  */
 void print_times_table(int n)
 {
-	int row, col;
+int row, col;
 
-	if (n < 0 || n > 15)
+if (n < 0 || n > 15)
 		return;
 
-	for (row = 0; row <= n; row++)
-	{
-		for (col = 0; col <= n; col++)
-		{
-			int result = row * col;
+for (row = 0; row <= n; row++)
+{
+for (col = 0; col <= n; col++)
+{
+int result = row * col;
 
-			if (col != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
+if (col != 0)
+{
+_putchar(',');
+_putchar(' ');
 
-				if (result < 10)
-					_putchar(' ');
+if (result < 10)
+_putchar(' ');
 
-				if (result < 100)
-					_putchar(' ');
+if (result < 100)
+_putchar(' ');
 			}
 
-			if (result < 10)
-				_putchar(result + '0');
-			else if (result < 100)
-			{
-				_putchar(result / 10 + '0');
-				_putchar(result % 10 + '0');
-			}
-			else
-			{
-				_putchar(result / 100 + '0');
-				_putchar((result / 10) % 10 + '0');
-				_putchar(result % 10 + '0');
-			}
-		}
+if (result < 10)
+_putchar(result + '0');
+else if (result < 100)
+{
+_putchar(result / 10 + '0');
+_putchar(result % 10 + '0');
+}
+else
+{
+_putchar(result / 100 + '0');
+_putchar((result / 10) % 10 + '0');
+_putchar(result % 10 + '0');
+}
+}
 
-		_putchar('\n');
-	}
+_putchar('\n');
+}
 }
 
