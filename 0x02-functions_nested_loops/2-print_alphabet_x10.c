f2d822ff56548481324f51a6ac78f1db25287663
Bilmage <nelsonnwafula@gmail.com>
2023-06-21 01:33:04 -0700

    update

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Program that prints alphabet in lower case 10  times
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+char c;
+int i = 0;
+
+while (i <= 10)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+i++;
+}
+}
+
