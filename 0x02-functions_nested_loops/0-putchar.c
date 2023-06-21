b604b06304c30a8f698aba6acd7374cd0b9075c3
Bilmage <nelsonnwafula@gmail.com>
2023-06-21 00:26:31 -0700

    update

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
+
 /**
  * main - Program that prints _putchar
  *
@@ -8,7 +9,17 @@
 
 int main(void)
 {
-printf("_putchar\n");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+   
 return (0);
+
 }
 
