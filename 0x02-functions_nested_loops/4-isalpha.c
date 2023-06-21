f71d325095a535341db239d8947bbc38fdf64e76
Bilmage <nelsonnwafula@gmail.com>
2023-06-21 02:38:15 -0700

    update

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,4 @@
-#include "main.c"
+#include "main.h"
 
 /**
  * _isalpha - Checks if a character is an alphabetic character.
@@ -8,9 +8,9 @@
  */
 int _isalpha(int c)
 {
-if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-return 1;
+if ((c >= '97' && c <= '122') || (c >= '65' && c <= '90'))
+return (1);
 else
-return 0;
+return (0);
 }
 
