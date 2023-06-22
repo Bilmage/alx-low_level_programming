2919345f6f505f0eb1b4edb9432e78b7c30cc38e
Nelson <34085153+Bilmage@users.noreply.github.com>
2023-06-22 22:31:11 +0300

    Update 3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,35 +9,35 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-    int is_leap_year = 0;
+int is_leap_year = 0;
 
-    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
-    {
-        is_leap_year = 1;
-    }
+if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+{
+is_leap_year = 1;
+}
 
-    int remaining_days;
+int remaining_days;
 
-    if (month == 2 && day == 29 && is_leap_year)
-    {
-        remaining_days = 366 - day;
-    }
-    else
-    {
-        int day_of_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        remaining_days = 365 - day;
+if (month == 2 && day == 29 && is_leap_year)
+{
+remaining_days = 366 - day;
+}
+else
+{
+int day_of_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+remaining_days = 365 - day;
 
-        if (is_leap_year)
-        {
-            day_of_month[2] = 29;
-        }
+if (is_leap_year)
+{
+day_of_month[2] = 29;
+}
 
-        for (int i = month + 1; i <= 12; i++)
-        {
-            remaining_days += day_of_month[i];
-        }
-    }
+for (int i = month + 1; i <= 12; i++)
+{
+remaining_days += day_of_month[i];
+}
+}
 
-    printf("There are %d days remaining in the year.\n", remaining_days);
+printf("There are %d days remaining in the year.\n", remaining_days);
 }
 
