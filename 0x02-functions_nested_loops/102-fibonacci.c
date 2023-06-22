1ca0f404deb76633f0c4932fd61ba24a35f9e33e
Nelson <34085153+Bilmage@users.noreply.github.com>
2023-06-22 11:42:46 +0300

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,28 +1,30 @@
 #include <stdio.h>
+
 /**
  * main - Entry point of the program
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 int fib[50];
 int i;
 
-fib[0] = 1;
-fib[1] = 2;
+fib[0] = 1; 
+fib[1] = 2; 
 
+    
 for (i = 2; i < 50; i++)
 {
-fib[i] = fib[i - 1] + fib[i - 2];
+fib[i] = fib[i - 1] + fib[i - 2]; 
 }
 
-for (i = 0; i < 50; i++)
+
+for (i = 0; i < 49; i++)
 {
-printf("%d ", fib[i]);
+printf("%d, ", fib[i]);
 }
-printf("\n");
+printf("%d\n", fib[49]); // Print the last Fibonacci number without the comma
 
-return (0);
+return 0;
 }
