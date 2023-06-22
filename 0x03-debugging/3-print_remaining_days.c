565f65453e01231ca8df90196053f14a5efc75fd
Bilmage <nelsonnwafula@gmail.com>
2023-06-22 12:25:33 -0700

    update

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -7,21 +7,37 @@
 * Return: 0
 */
 
-int main(void)
+void print_remaining_days(int month, int day, int year)
 {
-int month;
-int day;
-int year;
+    int is_leap_year = 0;
 
-month = 4;
-day = 01;
-year = 1997;
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+    {
+        is_leap_year = 1;
+    }
 
-printf("Date: %02d/%02d/%04d\n", month, day, year);
+    int remaining_days;
 
-day = convert_day(month, day);
+    if (month == 2 && day == 29 && is_leap_year)
+    {
+        remaining_days = 366 - day;
+    }
+    else
+    {
+        int day_of_month[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        remaining_days = 365 - day;
 
-print_remaining_days(month, day, year);
+        if (is_leap_year)
+        {
+            day_of_month[2] = 29;
+        }
 
-return (0);
+        for (int i = month + 1; i <= 12; i++)
+        {
+            remaining_days += day_of_month[i];
+        }
+    }
+
+    printf("There are %d days remaining in the year.\n", remaining_days);
 }
+
