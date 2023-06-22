31ef00d2878d4fa29e2798921cd4ffd7100f6e00
Nelson <34085153+Bilmage@users.noreply.github.com>
2023-06-22 22:34:26 +0300

    Update 3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include "main.h"
 
+
 /**
-* main - takes a date and prints how many days are left in the year, taking
-* leap years into account
-* Return: 0
-*/
+ * print_remaining_days - prints the number of remaining days in the year, accounting for leap years
+ * @month: the month (1-12)
+ * @day: the day of the month (1-31)
+ * @year: the year
+ *
+ * This function calculates the number of remaining days in the year, taking into account
+ * leap years. If the given date is February 29 in a leap year, it considers it as the
+ * last day of the year.
+ */
 
 void print_remaining_days(int month, int day, int year)
 {
