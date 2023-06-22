6b15986ce20dfc053075a35b70b43daa1a02053b
Bilmage <nelsonnwafula@gmail.com>
2023-06-21 20:26:39 -0700

    update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,45 +1,41 @@
-void print_number(int n) {
-    if (n == 0) {
-    putchar('0');
-    return;
-    }
+#include "main"
+#include <stdio.h>
+/**
+ * print_to_98 - prints all natural numbers from n to 98, followed by a new line.
+ */
 
-    if (n < 0) {
-    putchar('-');
-    n = -n;
-    }
-
-    int reversed = 0;
-    while (n > 0) {
-    reversed = reversed * 10 + (n % 10);
-    n /= 10;
-    }
-
-    while (reversed > 0) {
-    putchar('0' + (reversed % 10));
-    reversed /= 10;
-    }
-    }
-
-    void print_to_98(int n) {
-    if (n <= 98) {
-    for (int i = n; i <= 98; i++) {
-    print_number(i);
-    if (i != 98) {
-    putchar(',');
-    putchar(' ');
-    }
-    }
-    } 
-    else
-    {
-    for (int i = n; i >= 98; i--) {
-    print_number(i);
-    if (i != 98) {
-    putchar(',');
-    putchar(' ');
-    }
-    }
-    }
-    putchar('\n');
-    }
+void print_to_98(int n)
+{
+if (n <= 98)
+{
+for (; n <= 98; n++)
+{
+if (n == 98)
+{
+printf("%d", n);
+printf("\n");
+break;
+}
+else
+{
+printf("%d", n);
+}
+}
+}
+else
+{
+for (; n >= 98; n);
+{
+if (n == 98)
+{
+printf("%d", n);
+printf("\n");
+break;
+}
+else
+{
+printf("%d", n);
+}
+}
+}
+}
