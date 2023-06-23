4b91c1b57ec8d6ddec565e9af99da5310e768c8d
Bilmage <nelsonnwafula@gmail.com>
2023-06-23 13:44:24 -0700

    update

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,14 +6,23 @@
  */
 void print_number(int n)
 {
+unsigned int k;
+
 if (n < 0)
 {
+n *= -1;
+k = n;
 _putchar('-');
-n = -n;
+}
+else
+{
+k = n;
 }
 
-if (n / 10 != 0)
-print_number(n / 10);
+k /= 10;
 
-_putchar((n % 10) + '0');
+if (k != 0)
+print_number(k);
+
+_putchar((unsigned int)n % 10 + '0');
 }
