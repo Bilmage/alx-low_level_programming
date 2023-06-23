946abbc7e9906b2f2ffa62a87aa30199bb78f736
Nelson <34085153+Bilmage@users.noreply.github.com>
2023-06-23 22:19:27 +0300

    Update 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
+
 /**
  * _putchar - Custom putchar function
  * @c: Character to be printed
  *
  * Return: On success, return the character. On error, return EOF.
  */
-
 void _putchar(char c)
 {
 putchar(c);
 }
 
+/**
+ * print_diagonal - Prints a diagonal line on the terminal
+ * @n: Number of times the character '\' should be printed
+ */
 void print_diagonal(int n)
 {
 if (n <= 0)
@@ -30,8 +34,14 @@ _putchar('\n');
 }
 }
 
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on successful execution
+ */
 int main(void)
 {
-print_diagonal(5);  
-return (0);
+print_diagonal(5);  /* Example usage */
+return 0;
 }
+
