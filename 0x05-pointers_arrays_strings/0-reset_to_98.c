547f12c5637c3a9a6f7dc875d896ddb528cd4d8e
Bilmage <nelsonnwafula@gmail.com>
2023-06-27 05:50:57 -0700

    update

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Updates the value pointed to by an integer pointer to 98.
+ * @n: Pointer to the integer to be updated.
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
+
